2e04b7e174886a0adb3034a4409caf256e36cf27
Jane <glandea@icloud.com>
2016-05-31 12:36:30 +0300

    module2 task1
    Added restriction for line length.

--- build/js/game.js
+++ build/js/game.js
@@ -384,10 +384,11 @@
      * Отрисовка экрана паузы.
      */
     _drawPauseScreen: function() {
-      var message = {
-        stringValue: ['Я – Пендальф,', 'истребитель нечисти', 'Прыгнуть - пробел', 'стрелять – shift']
-      };
-      this.createText(message.stringValue);
+     // var message = {
+    //  stringValue: ['Я – Пендальф,', 'истребитель нечисти', 'Прыгнуть - пробел', 'стрелять – shift']
+     // };
+      var userMessage = 'Я  Пендальф – истребитель нечисти. Подпрыгнуть – пробел, пострелять – shift.';
+      this.cutText(userMessage);
       switch (this.state.currentStatus) {
         case Verdict.WIN:
           console.log('you have won!');
@@ -696,6 +697,34 @@
       for (var i = 0; i < textArray.length; i++) {
         this.ctx.fillText(textArray[i], OFFSET_X + 20, OFFSET_Y + 30 + 20 * i);
       }
+    },
+    cutText: function(stringToCut) {
+      this.ctx.fillStyle = 'rgba(0, 0, 0, 0.7)';
+      this.ctx.fillRect(OFFSET_X + 10, OFFSET_Y + 10, 250, 125);
+      this.ctx.fillStyle = '#FFFFFF';
+      this.ctx.fillRect(OFFSET_X, OFFSET_Y, 250, 125);
+      this.ctx.fillStyle = 'black';
+      this.ctx.font = '16px PT Mono';
+      var words = stringToCut.split(' ');
+      var countWords = words.length;
+      var line = '';
+      var x = OFFSET_X + 10;
+      var y = OFFSET_Y + 30;
+      var lineHeight = 20;
+      var maxWidth = 250;
+      for (var n = 0; n < countWords; n++) {
+        var testLine = line + words[n] + ' ';
+        var testWidth = this.ctx.measureText(testLine).width;
+        if (testWidth > maxWidth) {
+          this.ctx.fillText(line, x, y);
+          line = words[n] + ' ';
+          y += lineHeight;
+        }
+         else {
+          line = testLine;
+        }
+      }
+      this.ctx.fillText(line, x, y);
     }
   };
 
